0a424c2d445c1d52664587b49baec82905f5f006
HuynhTai488 <htai8400@gmail.com>
2024-11-28 01:05:22 +0700

    update train

--- train_model.py
+++ train_model.py
@@ -80,7 +80,7 @@ criterion = ContrastiveLoss(margin=1.0)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # Huấn luyện
-num_epochs = 10
+num_epochs = 100
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
