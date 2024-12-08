38490afa7aa572bac8984b28f43e19b178d25939
Hon Pg <96131758+Honpg@users.noreply.github.com>
2024-12-08 21:29:00 +0700

    push
    push

--- web/book_search.py
+++ web/book_search.py
@@ -11,18 +11,14 @@ class BookSearch:
         results = []
 
         for book in books:
-            # Lấy embedding từ cơ sở dữ liệu
             stored_img_embedding = np.array(book['image_embedding']).reshape(1, -1)
             stored_text_embedding = np.array(book['text_embedding']).reshape(1, -1)
 
-            # Tính độ tương đồng cosine
             img_similarity = cosine_similarity(img_embedding, stored_img_embedding)[0][0] if img_embedding is not None else 0
             text_similarity = cosine_similarity(text_embedding, stored_text_embedding)[0][0] if text_embedding is not None else 0
 
-            # Tính độ tương đồng tổng hợp
             overall_similarity = (img_similarity + text_similarity) / (2 if img_embedding is not None and text_embedding is not None else 1)
 
-            # Thêm sách và điểm tương đồng vào danh sách kết quả
             results.append({
                 "title": book["title"],
                 "price": book["price"],
@@ -30,5 +26,4 @@ class BookSearch:
                 "encoded_image": book["encoded_image"]
             })
 
-        # Sắp xếp kết quả theo độ tương đồng giảm dần và lấy 10 sách đầu tiên
         return sorted(results, key=lambda x: x['relevance_score'], reverse=True)[:10]
